3cca808846454e0575f575d4f04a9cf9d4c64597
Ümit Can Sariboga <ucsariboga@gmail.com>
2025-09-25 10:45:41 +0200

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        //Version 1
+        System.out.println("Hallo Git");
+        System.out.println("Git add . : staging area'ya alinir..");
+    }
+}
